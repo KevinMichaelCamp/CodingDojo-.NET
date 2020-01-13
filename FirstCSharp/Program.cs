36b4ed111d2ee9112e9e5427b409b925520e2bbc
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2020-01-12 20:52:31 -0600

    Fundamentals 1

--- FirstCSharp/Program.cs
+++ FirstCSharp/Program.cs
@@ -6,7 +6,18 @@ namespace FirstCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string first_name = "Kevin";
+            string last_name = "Camp";
+            string message = $"Hello {first_name} {last_name}!";
+            Console.WriteLine(message);
+            Console.WriteLine("My name is {0}, I am " + 28 + " years old", "Tim");
+            // Random numbers / loops
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(rand.Next(0, 10));
+            }
+
         }
     }
 }
---
+++ Fundamentals/Fun1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Fun1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Loop that prints all values from 1-255
+            for (int i = 1; i <= 255; i++)
+            {
+                Console.WriteLine(i);
+            }
+            // Create a new loop that prints all values from 1-100 that are divisible by 3 or 5, but not both
+            for (int j = 1; j <= 100; j++)
+            {
+                if ((j % 3 == 0) && (j % 5 != 0))
+                {
+                    Console.WriteLine(j + "Fizz");
+                }
+                else if ((j % 3 != 0) && (j % 5 == 0))
+                {
+                    Console.WriteLine(j + "Buzz");
+                }
+                else if ((j % 3 == 0) && (j % 5 == 0))
+                {
+                    Console.WriteLine(j + "FizzBuzz");
+                }
+            }
+            // Use while loop
+            int k = 1;
+            while (k <= 100)
+            {
+                if ((k % 3 == 0) && (k % 5 != 0))
+                {
+                    Console.WriteLine(k + "Fizz");
+                }
+                else if ((k % 3 != 0) && (k % 5 == 0))
+                {
+                    Console.WriteLine(k + "Buzz");
+                }
+                else if ((k % 3 == 0) && (k % 5 == 0))
+                {
+                    Console.WriteLine(k + "FizzBuzz");
+                }
+                k++;
+            }
+        }
+    }
+}
