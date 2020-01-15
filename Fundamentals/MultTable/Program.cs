a6c2e5e07084820ff473d29c4e01606a9dd81c48
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2020-01-15 15:13:57 -0600

    Mult Table

---
+++ Fundamentals/MultTable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MultTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] multArray = new int[10, 10];
+            for (int row = 0; row < 10; row++)
+            {
+                for (int col = 0; col < 10; col++)
+                {
+                    multArray[row, col] = (row + 1) * (col + 1);
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(multArray[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
